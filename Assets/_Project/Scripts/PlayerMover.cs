28969acb37d124209a646b8fdddc218507568f42
kamiy2743 <kamiy2743@gmail.com>
2022-09-10 00:13:27 +0900

    プレイヤーを動かす

--- Assets/_Project/Scripts/PlayerMover.cs
+++ Assets/_Project/Scripts/PlayerMover.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMover : MonoBehaviour
 {
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private float moveSpeed;
     private Rigidbody rigidbody;
+
+    private InputAction moveAction;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        moveAction = playerInput.currentActionMap["Move"];
     }
 
     // Update is called once per frame
     void Update()
     {
+        var inputVector = moveAction.ReadValue<Vector2>().normalized;
+        var moveVector = new Vector3(inputVector.x * moveSpeed, -9.8f, inputVector.y * moveSpeed);
 
+        rigidbody.velocity = moveVector;
     }
 }
