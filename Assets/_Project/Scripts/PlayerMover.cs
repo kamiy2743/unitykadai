5934de6e6f7de240452141804caafaa7f28ea7af
kamiy2743 <kamiy2743@gmail.com>
2022-09-10 00:48:16 +0900

    カメラを動かす

--- Assets/_Project/Scripts/PlayerMover.cs
+++ Assets/_Project/Scripts/PlayerMover.cs
@@ -2,26 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class PlayerMover : MonoBehaviour
 {
     [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private CinemachineVirtualCamera playerCamera;
     [SerializeField] private float moveSpeed;
     private Rigidbody rigidbody;
 
     private InputAction moveAction;
+    private CinemachinePOV cinemachinePOV;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         moveAction = playerInput.currentActionMap["Move"];
+        cinemachinePOV = playerCamera.GetCinemachineComponent(CinemachineCore.Stage.Aim).GetComponent<CinemachinePOV>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var inputVector = moveAction.ReadValue<Vector2>().normalized;
-        var moveVector = new Vector3(inputVector.x * moveSpeed, -9.8f, inputVector.y * moveSpeed);
+        var horizontalMoveVector = moveAction.ReadValue<Vector2>().normalized * moveSpeed;
+        var horizontalRotation = Quaternion.Euler(0, cinemachinePOV.m_HorizontalAxis.Value, 0);
+        var moveVector = horizontalRotation * new Vector3(horizontalMoveVector.x, -9.8f, horizontalMoveVector.y);
 
         rigidbody.velocity = moveVector;
     }
