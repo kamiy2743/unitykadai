2835199f5088d18e67ceac3e0ce775da89e63b54
kamiy2743 <kamiy2743@gmail.com>
2022-09-11 19:43:12 +0900

    アイテムのカウントをするUI

--- Assets/_Project/Scripts/KeyItemManager.cs
+++ Assets/_Project/Scripts/KeyItemManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class KeyItemManager : MonoBehaviour
 {
     [SerializeField] private PlayerDetector keyItemPrefab;
+    [SerializeField] private TMP_Text itemCountUI;
 
     public List<GameObject> KeyItems = new List<GameObject>();
 
+    private int maxItemCount;
     private int keyItemCount;
 
     public void StartInitial(List<Vector3> startPositions)
@@ -20,15 +23,25 @@ public class KeyItemManager : MonoBehaviour
             keyItem.OnDetect.AddListener(() => OnGet(keyItem.gameObject));
             KeyItems.Add(keyItem.gameObject);
         }
+
+        maxItemCount = keyItemCount;
+        SetText();
     }
 
     private void OnGet(GameObject keyItem)
     {
         keyItem.SetActive(false);
         keyItemCount -= 1;
+        SetText();
+
         if (keyItemCount <= 0)
         {
             Debug.Log("clear");
         }
+    }
+
+    private void SetText()
+    {
+        itemCountUI.text = $"封印を解くカギ: {maxItemCount - keyItemCount} / {maxItemCount}";
     }
 }
